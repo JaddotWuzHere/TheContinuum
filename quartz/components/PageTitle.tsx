e52bfcae19d4fa96328a3b819d37ca607d6e460f
jaddotwuzhere <jaddotplayz@gmail.com>
2026-03-07 22:12:52 -0500

    Added text logo

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -1,23 +1,41 @@
 import { pathToRoot } from "../util/path"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import { classNames } from "../util/lang"
-import { i18n } from "../i18n"
 
-const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
-  const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
+const PageTitle: QuartzComponent = ({ fileData, displayClass }: QuartzComponentProps) => {
   const baseDir = pathToRoot(fileData.slug!)
+
   return (
     <h2 class={classNames(displayClass, "page-title")}>
-      <a href={baseDir}>{title}</a>
+      <a href={baseDir} class="page-title-link" aria-label="The Continuum home">
+        <img src={`${baseDir}/static/text.png`} alt="The Continuum" class="page-title-logo" />
+      </a>
     </h2>
   )
 }
 
 PageTitle.css = `
 .page-title {
-  font-size: 1.75rem;
   margin: 0;
-  font-family: var(--titleFont);
+  width: 100%;
+  overflow: visible;
+}
+
+.page-title-link {
+  display: block;
+  width: 100%;
+  text-decoration: none;
+  overflow: visible;
+}
+
+.page-title-logo {
+  display: block;
+  width: 350px;
+  max-width: none;
+  height: auto;
+  margin: 0;
+  border-radius: 0;
+  transform: translateX(-6rem);
 }
 `
 
